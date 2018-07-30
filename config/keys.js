bc9f1a16c03a5dd90c24ce9db013e79e9efe1028
Armen <armen.chorbajian@gmail.com>
2018-07-30 14:35:17 -0700

    Backend Login finished

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://armenchorb:moles2@ds011963.mlab.com:11963/devconnector"
+  mongoURI: "mongodb://armenchorb:moles2@ds011963.mlab.com:11963/devconnector",
+  secretOrKey: "secret"
 };
