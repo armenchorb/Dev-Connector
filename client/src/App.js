5950220e37f2d4056f4f631222d27cbca0374d43
Armen <armen.chorbajian@gmail.com>
2018-08-05 15:40:40 -0700

    Post Feed components created, halfway complete

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
 import store from "./store";
 
 import PrivateRoute from "./components/common/PrivateRoute";
@@ -87,6 +88,9 @@ class App extends Component {
                   path="/edit-profile"
                   component={EditProfile}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
