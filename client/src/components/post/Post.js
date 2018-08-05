5af86c3884e9ac9f2f83ab8af89ecfd2ea31b987
Armen <armen.chorbajian@gmail.com>
2018-08-05 16:58:48 -0700

    comments complete with likes and errors

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import Spinner from "../common/Spinner";
 import CommentForm from "./CommentForm";
+import CommentFeed from "./CommentFeed";
 import { getPost } from "../../actions/postActions";
 import PostItem from "../posts/PostItem";
 
@@ -23,6 +24,7 @@ class Post extends Component {
         <div>
           <PostItem post={post} showActions={false} />
           <CommentForm postId={post._id} />
+          <CommentFeed postId={post._id} comments={post.comments} />
         </div>
       );
     }
